a1c74ce3e0c723ae52f46485a561f4007c8e953d
Matthew Crossley <matthew.crossley@bp.com>
2022-10-17 13:43:31 +0100

    Add script to create tables

---
+++ CreateSchema.sql
@@ -0,0 +1,23 @@
+use bookish
+go
+
+create table BookLoans(
+    ISBN int IDENTITY(1,1) not null primary key,
+    UserId int null,
+    Due DATE NULL
+)
+
+create table Books(
+    ISBN int IDENTITY(1,1) not null primary key,
+    Title varchar(255) not null,
+    Author varchar(255) not null
+)
+
+create table Users(
+    UserId int IDENTITY(1,1) not null primary key,
+    Username varchar(128) not null,
+    Pass varchar(512) not null,
+    Salt varchar(256) not null
+)
+
+go
