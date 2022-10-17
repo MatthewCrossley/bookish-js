a62094f7025205a22b9bb894a1232dd106bd6f2d
Matthew Crossley <matthew.crossley@bp.com>
2022-10-17 15:11:30 +0100

    Better tables

--- CreateSchema.sql
+++ CreateSchema.sql
@@ -2,19 +2,23 @@ use bookish
 go
 
 create table Checkouts(
-    ISBN int IDENTITY(1,1) not null primary key,
+    CopyId int not null,
     UserId int null,
     CheckoutDate date not null,
     DueDate date not null,
     ReturnDate date null
 )
-go
 
 create table Books(
     BookId int IDENTITY(1,1) not null primary key,
     Title varchar(255) not null,
     Author varchar(255) not null,
     ISBN varchar(14)
+)
+
+create table Copies(
+    CopyId int IDENTITY(1,1) not null primary key,
+    BookId int not null
 )
 
 create table Users(
